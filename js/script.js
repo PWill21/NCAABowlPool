bc43366c5ba263baccc456c9dda68b9a63856283
Patrick J. Will <patrickwill2121@gmail.com>
2014-07-21 01:44:36 -0400

    changed .js to revert back to correct background color of table after hover exit

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 	// change background color when mouse hovers over team names
 	changeRowColor();
 	changeButtonColor();
+	fadeInOnHover();
 });
 
 /****************Functions*****************************************/
@@ -10,7 +11,7 @@ function changeRowColor() {
 	$("tr").hover(function(){
   		$(this).css("background-color","#4DB9DD");
   	},function(){
-  		$(this).css("background-color","#fff");
+  		$(this).css("background-color","#F2F9FC");
 	});
 }
 
@@ -21,6 +22,15 @@ function changeButtonColor() {
 			$(this).css("background-color","#ABDAEA");
 	});
 }
+/*
+function fadeInOnHover() {
+	$("radio").hover(function() {
+		$(this).fadeTo("slow", 1.0);
+	},function() {
+		$(this).fadeTo("slow", 0);
+	});
+}
+*/
 /*
 function changeTeamBackground() {
 	$("td").mouseover(function() {
